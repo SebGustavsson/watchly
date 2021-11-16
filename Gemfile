e93721d54166fb663ac3edca94aa314329140184
Jens Malmlöf <malmlof91@hotmail.com>
2021-11-16 14:54:43 +0100

    created watches controller and view

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 gem 'dotenv-rails', groups: [:development, :test]
+gem 'cloudinary', '~> 1.16.0'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,12 +1,28 @@
 class WatchesController < ApplicationController
+
+  def index
+    @watch = Watch.all
+  end
+
+  def show
+    @watch = Watch.find(params[:id])
+  end
+
   def new
     @watch = Watch.new
   end
 
   def create
     @watch = Watch.new(watch_params)
-    @watch.list = @list
-    @watch.save
-    redirect_to watch_path(@list)
+    @watch.user = current_user
+    if @watch.save
+    redirect_to watch_path(@watch)
+    else
+      render :new
+    end
+  end
+
+  def watch_params
+  params.require(:watch).permit(:model, :year, :brand, photos: [])
   end
 end
