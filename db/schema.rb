9d1faa261c66cec0ea9a7c13a8256408668e4da5
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-17 13:02:09 +0100

    added map to index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_103118) do
+ActiveRecord::Schema.define(version: 2021_11_17_105826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,9 @@ ActiveRecord::Schema.define(version: 2021_11_16_103118) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "photo_url"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_watches_on_user_id"
   end
 
