5c3feaaf067b472b2f921434393b8984ad36be51
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-18 15:54:30 +0100

    added price to the seed

--- app/models/watch.rb
+++ app/models/watch.rb
@@ -2,6 +2,7 @@ class Watch < ApplicationRecord
   belongs_to :user
   validates :brand, :model, :year, presence: true
   has_many_attached :photos
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
