ee03d37cf674e6cf5f8dbca6834e7e047a381794
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-18 14:29:19 +0100

    created bookings function as well as small UI changes

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,25 @@
+class BookingsController < ApplicationController
+  def new
+    @watch = Watch.find(params[:watch_id])
+    @booking = Booking.new
+    @booking.watch = @watch
+  end
+
+  def create
+    @watch = Watch.find(params[:watch_id])
+    @booking = Booking.new(booking_params)
+    @booking.watch = @watch
+    @booking.user = current_user
+    if @booking.save
+      redirect_to watch_path(@watch), notice: "You have now booked the watch"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :watch_id)
+  end
+end
