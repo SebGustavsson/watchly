c392a7493a4ebab64c63e28ad8c338f196143d5c
John Delgado <j.delgado828@gmail.com>
2022-03-07 18:11:31 +0100

    added mapbox

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:destroy]
   patch '/accept', to: 'bookings#accept', as: :accept
   patch '/reject', to: 'bookings#reject', as: :reject
 end
